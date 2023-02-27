c41c3efaa01b8b15da87cab4fa5bc7570abf28c8
artursolekss <arutrs.olekss@gmail.com>
2023-02-27 20:21:01 +0200

    27/02 (1)

---
+++ VarDataTypesInput/VariablesDemo.py
@@ -0,0 +1,26 @@
+# name = "Arturs"
+# print(name)
+# name = "Janis"
+# print(name)
+# age = 25
+# print(age)
+
+# name = 30
+# print(name)
+
+# Variables are case-sensitive in Python
+# variable = "323232"
+# Variable = 1111
+
+# print(Variable)
+
+# Will trigger the error, because the types are different
+# x = 5
+# y = "9"
+# sum = x + y
+# print(sum)
+
+x = 5
+y = "9"
+sum = str(x) + y #str does the conversion to string
+print(sum)
